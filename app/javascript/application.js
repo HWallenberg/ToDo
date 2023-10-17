d3d2ea8af58626bd40c622db5e891e7fd35c0d5c
Hadleigh Wallenberg <hadleigh.wallenberg@transformuk.com>
2023-10-17 09:19:36 +0100

    updated javascript file imports

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-//= require bootstrap
-//= require bootstrap/alert
-//= require bootstrap/button
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
 import "@hotwired/turbo-rails"
 import "controllers"
